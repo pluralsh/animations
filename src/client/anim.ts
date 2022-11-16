bc23d6dde1935cbc314a56d424daeeff0bda9917
Klink <85062+dogmar@users.noreply.github.com>
2022-11-16 15:45:52 -0800

    Basic gsap animation working

--- src/client/anim.ts
+++ src/client/anim.ts
@@ -1,4 +1,4 @@
-import gsap from 'gsap'
+import gsap from "gsap";
 
 export default (() => {
   console.log("stuff");
@@ -7,3 +7,33 @@ export default (() => {
   });
 })();
 
+const tl = gsap.timeline({ repeat: -1, repeatDelay: 2 });
+tl.addLabel("start", 0);
+
+tl.from(".bigSquareContainer", { opacity: 0, duration: 0.5 }, "start");
+tl.from(
+  ".bigSquareArea",
+  {
+    translateY: "-30%",
+    rotateY: "+=30",
+    rotateX: "-=5",
+    rotateZ: "-=5",
+    duration: 1,
+    ease: "sine.inOut",
+  },
+  "start"
+);
+tl.from(
+  ".appSquare .zTarget",
+  {
+    z: 60,
+    opacity: 0,
+    duration: 1,
+    stagger: {
+      grid: [3, 3],
+      from: "edges",
+      amount: 0.5,
+    },
+  },
+  "<0.2"
+);
