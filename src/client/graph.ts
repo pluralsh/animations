2ad25df2a2a29cb0fb84d2f631564ab63de94be9
Klink <85062+dogmar@users.noreply.github.com>
2022-12-13 11:35:45 -0800

    cleanup

--- src/client/graph.ts
+++ src/client/graph.ts
@@ -12,7 +12,7 @@ export const data = [
   {
     color: "#5CC3FF",
     ceiling: 40,
-    floor: 0,
+    floor: 5,
   },
 ];
 
@@ -23,15 +23,24 @@ function easeOutCubicReverse(x: number): number {
   return 1 - Math.pow(x, 3);
 }
 
-function powerDist(x: number, pow: number = 2.5): number {
+function powerDist(x: number, power: number = 2): number {
   if (x < 0.5) {
-    return 1 - Math.pow(1 - x * 2, 2.5);
+    return 1 - Math.pow(1 - x * 2, power);
   } else {
-    return 1 - Math.pow((x - 0.5) * 2, 2.5);
+    return 1 - Math.pow((x - 0.5) * 2, power);
   }
 }
-export type Node = [number, number];
-export type NodeList = Node[];
+
+// function sinDist(x, _): number {
+//   return (Math.sin(x * Math.PI * 2 + 1.5 * Math.PI) + 1) / 2;
+// }
+
+function graphScalingFunc(x: number): number {
+  return powerDist(x, 2);
+}
+
+export type GraphNode = [number, number];
+export type GraphNodeList = GraphNode[];
 
 type NodeOptions = {
   nodeCount: number;
@@ -45,41 +54,33 @@ const generateNode = (
   i,
   { nodeCount, spacing, floor, ceiling, xPadding }: NodeOptions
 ): [number, number] => {
-  return [
-    xPadding + spacing * i,
- Math.random() * (ceiling - floor) + floor,
-  ];
+  return [xPadding + spacing * i, Math.random() * (ceiling - floor) + floor];
 };
 
-export function generateLineNodes(opts: NodeOptions): NodeList {
+export function generateLineNodes(opts: NodeOptions): GraphNodeList {
   const { nodeCount } = opts;
-  const nodes: NodeList = new Array(nodeCount);
+  const nodes: GraphNodeList = new Array(nodeCount);
   for (let i = 0; i < nodeCount; ++i) {
     nodes[i] = generateNode(i, opts);
   }
-  console.log("nodes", nodes);
   return nodes;
 }
 
-export function updateLineNodes(nodes: NodeList, opts: NodeOptions): NodeList {
+export function updateLineNodes(nodes: GraphNodeList, opts: NodeOptions): GraphNodeList {
   return nodes.map((node, i) => {
     if (i === nodes.length - 1) {
-      console.log("new node opts", opts);
-
       const node = generateNode(i, opts);
-      console.log("new node", node);
       return node;
     }
     return [node[0], nodes[i + 1][1]];
   });
 }
 
-export function constructPath(vals: NodeList) {
+export function constructPath(vals: GraphNodeList) {
   const len = vals.length;
   return vals.reduce((str, val, i) => {
     const indexRatio = i / (len - 1);
-    const multiplier = powerDist(indexRatio);
-    console.log("val", val);
+    const multiplier = graphScalingFunc(indexRatio);
     const x = val[0];
     const y = val[1];
     const scaledY = y * multiplier;
