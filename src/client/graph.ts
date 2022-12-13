41cbf5deb5b7b3cec6578b6740c172e8cf7cce83
Klink <85062+dogmar@users.noreply.github.com>
2022-12-13 11:09:44 -0800

    super basic graph animation happening

--- src/client/graph.ts
+++ src/client/graph.ts
@@ -6,45 +6,84 @@ export const data = [
   },
   {
     color: "#3CECAF",
-    ceiling: 65,
+    ceiling: 70,
     floor: 10,
   },
   {
     color: "#5CC3FF",
-    ceiling: 30,
+    ceiling: 40,
     floor: 0,
   },
 ];
 
-export function generateLineNodes({
-  nodeCount,
-  spacing,
-  floor,
-  ceiling,
-  xPadding,
-}: {
+function easeOutCubic(x: number): number {
+  return 1 - Math.pow(1 - x, 3);
+}
+function easeOutCubicReverse(x: number): number {
+  return 1 - Math.pow(x, 3);
+}
+
+function powerDist(x: number, pow: number = 2.5): number {
+  if (x < 0.5) {
+    return 1 - Math.pow(1 - x * 2, 2.5);
+  } else {
+    return 1 - Math.pow((x - 0.5) * 2, 2.5);
+  }
+}
+export type Node = [number, number];
+export type NodeList = Node[];
+
+type NodeOptions = {
   nodeCount: number;
   floor: number;
   ceiling: number;
   spacing: number;
   xPadding: number;
-}) {
-  const nodes: [number, number][] = new Array(nodeCount);
+};
+
+const generateNode = (
+  i,
+  { nodeCount, spacing, floor, ceiling, xPadding }: NodeOptions
+): [number, number] => {
+  return [
+    xPadding + spacing * i,
+ Math.random() * (ceiling - floor) + floor,
+  ];
+};
+
+export function generateLineNodes(opts: NodeOptions): NodeList {
+  const { nodeCount } = opts;
+  const nodes: NodeList = new Array(nodeCount);
   for (let i = 0; i < nodeCount; ++i) {
-    nodes[i] = [
-      xPadding + (spacing * i),
-      i === 0 || i === nodeCount - 1
-        ? 0
-        : Math.random() * (ceiling - floor) + floor,
-    ];
+    nodes[i] = generateNode(i, opts);
   }
+  console.log("nodes", nodes);
   return nodes;
 }
 
-export function constructPath(vals: [number, number][]) {
-  return vals.reduce((str, val, idx) => {
-    return `${str} ${idx === 0 ? "M" : idx === 1 ? "L" : ""}${val[0]},${
-      val[1]
-    }`;
+export function updateLineNodes(nodes: NodeList, opts: NodeOptions): NodeList {
+  return nodes.map((node, i) => {
+    if (i === nodes.length - 1) {
+      console.log("new node opts", opts);
+
+      const node = generateNode(i, opts);
+      console.log("new node", node);
+      return node;
+    }
+    return [node[0], nodes[i + 1][1]];
+  });
+}
+
+export function constructPath(vals: NodeList) {
+  const len = vals.length;
+  return vals.reduce((str, val, i) => {
+    const indexRatio = i / (len - 1);
+    const multiplier = powerDist(indexRatio);
+    console.log("val", val);
+    const x = val[0];
+    const y = val[1];
+    const scaledY = y * multiplier;
+
+    return `${str} ${i === 0 ? "M" : i === 1 ? "L" : ""}${x},${scaledY}`;
   }, "");
 }
